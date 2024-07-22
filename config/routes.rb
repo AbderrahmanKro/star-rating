f2105b08673d801889f802ae4b1a4751f047b47b
AbderrahmanKro <elhairechabderrahman@gmail.com>
2024-07-22 16:14:53 +0100

    add controller and stimulus

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users do
-    resources :reviews, only: [:index, :new, :create]
-  end
 
-  resources :reviews, only: [:show, :edit, :update, :destroy]
+  resources :reviews, only: [:new, :create]
   # Defines the root path route ("/")
   # root "articles#index"
 end
